ae2d5991663adcc82691482c6d618128487c89f4
Daniel Herrmann <dherrmann@addval.com>
2021-11-12 21:08:54 -0300

    tests with files and downloads

--- fuensalida_flask_app.py
+++ fuensalida_flask_app.py
@@ -1,8 +1,10 @@
-from clientes.analisis_fuensalida.informe_clientes_nacionales import informe_cuentas_por_cobrar_fuensalida
-from datetime import datetime
+# from clientes.analisis_fuensalida.informe_clientes_nacionales import informe_cuentas_por_cobrar_fuensalida
+# from datetime import datetime
+import pandas as pd
+import os
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request,send_file
 # import datetime
 
 app = Flask(__name__)
@@ -12,20 +14,19 @@ app.config["DEBUG"] = True
 def hello():
     return render_template("html_user_interface.html")
 
-@app.route("/test")
+@app.route("/generate-report")
 def buttons():
-    input_date = datetime.strptime(request.args["fecha"],"%d/%m/%Y")
-    # informe_cuentas_por_cobrar_fuensalida(input_date)
-    return str(input_date)
-    # return render_template("html_user_interface.html")
-    # return request.args
-    # input_date = request.form["input-fecha"]
-    # flash('This is a flash me', 'success')
-    # print(str(input_date)+" - analisis_clientes_nacionales")
+    input_date = request.args["fecha"]
+    informe = request.args["informe"]
 
-# @app.route("/test")
-# def analisis_reembolsos():
-#     # input_date = request.form["input-fecha"]
-#     flash('This is a flash success message', 'success')
-#     # print(str(input_date)+" - analisis_reembolsos")
-#     return "/analisis_reembolsos"
+    output_df = pd.DataFrame([[0,1,2,3],[0,1,2,3],[0,1,2,3]])
+    output_df.iloc[1,:] = input_date
+    path = os.path.dirname(os.path.realpath(__file__))
+
+    if informe=="clientes-nacionales":
+        output_df.iloc[0,:] = informe
+    elif informe=="reembolsos":
+        output_df.iloc[2,:] = informe
+
+    output_df.to_excel(path+"/fake_excel.xlsx")
+    return send_file(path+"/fake_excel.xlsx", as_attachment=False)
